85cee8c0714352fd3a15f9ab2f3c2dba66f54b06
Thomas Michelbach <thomasmichelbach@users.noreply.github.com>
2024-04-15 07:31:02 +0200

    Delete dist/react directory

--- dist/react/AddL.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-const SvgAddL = () => (
-  <svg width="1em" height="1em" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path fill="currentColor" d="M23 23V11h1v12h12v1H24v12h-1V24H11v-1h12Z" />
-  </svg>
-);
-export default SvgAddL;
--- dist/react/BookmarkActiveS.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-const SvgBookmarkActiveS = () => (
-  <svg width="1em" height="1em" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path fill="currentColor" d="M5 0h14v23.7l-7-7-7 7V0Z" />
-  </svg>
-);
-export default SvgBookmarkActiveS;
